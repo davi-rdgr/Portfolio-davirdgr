3cf8b34d3df68126f28ca5ba46395fde01f48b8c
davi-rdgr <davirdgrsouza@gmail.com>
2025-02-06 01:03:59 -0300

    feat: skills update, about me update, certifies update

--- javascript/index.js
+++ javascript/index.js
@@ -12,7 +12,7 @@ const list = ['<a href="#ID_section_intro">Inicio</a>', '<a href="#ID_about_me">
 document.addEventListener('DOMContentLoaded', function () {
     // apresentação
     new Typewriter(typewriter, {
-        strings: ['<p class="ap_p"><span class="ap-const">const</span> <span class="ap-yellow">Davi_Rodeghiero_Souza</span> <span class="ap-const">=</span> <span class="ap-key">{</span><br> <span class="ap-tab">name:</span> <span class="ap-yellow">"Davi Rodeghiero Souza"</span>,<br> <span class="ap-tab">age:</span> <span class="ap-purple">24</span>, <br> <span class="ap-tab">city:</span> <span class="ap-yellow">"Morro Redondo - RS"</span>,<br> <span class="ap-tab">faculty:</span> <span class="ap-key">{</span><br>  <span class="ap-doubletab">Pucrs:</span> <span class="ap-yellow"> "Análise e Desenvolvimento de Sistemas"</span>,<br> <span class="ap-doubletab">Senac:</span> <span class="ap-yellow">"Técnico em Desenvolvimento de Sistemas"</span>,<br> <span class="ap-tab"> <span class="ap-key">}</span></span> <br> <span class="ap-key">}</span> </p>'],
+        strings: ['<p class="ap_p"><span class="ap-const">const</span> <span class="ap-yellow">Davi_Rodeghiero_Souza</span> <span class="ap-const">=</span> <span class="ap-key">{</span><br> <span class="ap-tab">name:</span> <span class="ap-yellow">"Davi Rodeghiero Souza"</span>,<br> <span class="ap-tab">age:</span> <span class="ap-purple">24</span>, <br> <span class="ap-tab">city:</span> <span class="ap-yellow">"Morro Redondo - RS"</span>,<br> <span class="ap-tab">faculty: <span class="ap-yellow">"Senac - Técnico em Desenvolvimento de Sistemas"</span> <span class="ap-tab"></span> <br> <span class="ap-key">}</span>;</p>'],
         autoStart: true,
         delay: 10,
         loop: true,
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     new Typewriter(typewriterp, {
-        strings: ['<p>&lt;<span style="color: var(--rosa);">p</span>&gt; Me chamo Davi Rodeghiero Souza, tive meu primeiro contato com a programação em 2018 com um curso de  Algoritmo usando a ferramenta Studio Portugol. &lt;/<span style="color: var(--rosa);">p</span>&gt; </p> <p>&lt;<span style="color: var(--rosa);">p</span>&gt; Carrego alguns certificados de conclusão e  atualmente estou cursando Análise e  Desenvolvimento de Sistemas. &lt;/<span style="color: var(--rosa);">p</span>&gt;</p>'],
+        strings: ['<p>&lt;<span style="color: var(--rosa);">p</span>&gt; Me chamo Davi Rodeghiero Souza, tive meu primeiro contato com a programação em 2018 com um curso de  Algoritmo usando a ferramenta Studio Portugol, mas ingressei oficialmente em 2022, focando inicialmente em Desenvolvimento Web &lt;/<span style="color: var(--rosa);">p</span>&gt; </p> <p>&lt;<span style="color: var(--rosa);">p</span>&gt; Carrego alguns certificados de conclusão e  atualmente estou cursando Análise e  Desenvolvimento de Sistemas. &lt;/<span style="color: var(--rosa);">p</span>&gt;</p>'],
         autoStart: true,
         delay: 10,
         loop: true,
